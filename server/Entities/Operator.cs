c1286eb4fdb68d5ebaff5d6b6105bd9080e2d879
DitiJ3 <eruditj@gmail.com>
2024-04-20 18:18:00 +0200

    Create the Operator entity

--- server/Entities/Operator.cs
+++ server/Entities/Operator.cs
@@ -1,7 +1,7 @@
 namespace server.Entities
 {
-    // Defining the City Entity with attributes same as the database table
-    public class City
+    // Defining the Operator Entity with attributes same as the database table
+    public class Operator
     {
         public int Id { get; set; }
         public string Name { get; set; }
