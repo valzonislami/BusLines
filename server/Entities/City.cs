10a4cbffff8d50d4cc4b9ffd6d93f7b4db8fca5b
DitiJ3 <eruditj@gmail.com>
2024-04-20 18:11:10 +0200

    Create the City Entity

---
+++ server/Entities/City.cs
@@ -0,0 +1,9 @@
+namespace server.Entities
+{
+    // Defining the City Entity with attributes same as the database table
+    public class City
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
