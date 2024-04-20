d644802d4e621128de2b9ff486f24bbaca5b15f3
Valzon <valzonislami@gmail.com>
2024-04-20 21:42:15 +0200

    Create 'Ticket' Entity

--- server/DataAccess/BusDbContext.cs
+++ server/DataAccess/BusDbContext.cs
@@ -12,13 +12,15 @@ namespace server.DataAccess
             _configuration = configuration;
         }
 
-
         public DbSet<City> Cities { get; set; }
         public DbSet<Operator> Operators { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<BusLine> BusLines { get; set; }
         public DbSet<BusSchedule> BusSchedules { get; set; }
         public DbSet<Stop> Stops { get; set; }
         public DbSet<BusScheduleStop> BusScheduleStops { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //Contiguring many-to-many relationship between BusSchedule and Stop
@@ -37,7 +39,6 @@ namespace server.DataAccess
 
             base.OnModelCreating(modelBuilder);
         }
-
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             {
---
+++ server/Entities/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace server.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        // Foreign key for UserId
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        // Foreign key for BusScheduleId
+        public int BusScheduleId { get; set; }
+        [ForeignKey("BusScheduleId")]
+        public BusSchedule BusSchedule { get; set; }
+        public int Seat { get; set; }
+        public DateTime DateOfBooking { get; set; }
+
+    }
+}
