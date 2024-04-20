7d27a602f05c71ee8380a39534fb81c31dcb4056
dionaorllani <diona.orllani@gmail.com>
2024-04-20 18:27:52 +0200

    Create "BusLine" Entity

---
+++ server/Entities/BusLine.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace server.Entities
+{
+    public class BusLine
+    {
+        public int Id { get; set; }
+
+        // Foreign key for the start city
+        public int StartCityId { get; set; }
+        [ForeignKey("StartCityId")]
+        public City StartCity { get; set; }
+
+        // Foreign key for the destination city
+        public int DestinationCityId { get; set; }
+        [ForeignKey("DestinationCityId")]
+        public City DestinationCity { get; set; }
+    }
+}
