5633a8c449b143ce7f64fd7d07daafc6bd530ff7
Valzon <valzonislami@gmail.com>
2024-04-21 21:27:00 +0200

    Create 'BusScheduleDTO'

---
+++ server/Models/BusScheduleDTO.cs
@@ -0,0 +1,25 @@
+﻿using server.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace server.Models
+{
+    public class BusScheduleDTO
+    {
+        // Foreign key for BusLineId
+        public string StartCityName { get; set; }
+        public string DestinationCityName { get; set; }
+
+        // Foreign key for OperatorId
+
+        public string OperatorName { get; set; }
+
+        public DateTime Departure { get; set; }
+
+        public DateTime Arrival { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
