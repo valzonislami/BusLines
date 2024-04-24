2e8c1246574be3b8a8b5fa74968edf54625796e9
Valzon <valzonislami@gmail.com>
2024-04-24 22:35:59 +0200

    Design Login Page

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -30,17 +30,24 @@ const LogIn = () => {
     };
 
     return (
-        <div>
-            {error && <div>{error}</div>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input type="email" id="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <div className="min-h-screen flex justify-center items-center bg-orange-50">
+            <div className="max-w-md w-full">
+                <div className="bg-white py-8 px-6 rounded-lg shadow-lg bg-opacity-90">
+                    <h1 className="text-3xl font-semibold text-center text-orange-400 mb-8">Welcome back!</h1>
+                    {error && <div className="mb-4 text-orange-400">{error}</div>}
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-6">
+                            <input className="w-full px-4 py-3 rounded-md bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:border-orange-500 focus:bg-white transition duration-300 ease-in-out" type="email" id="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        </div>
+                        <div className="mb-6">
+                            <input className="w-full px-4 py-3 rounded-md bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:border-orange-500 focus:bg-white transition duration-300 ease-in-out" type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        </div>
+                        <button className="w-full bg-orange-400 hover:bg-orange-500 py-3 rounded-md text-white font-semibold transition duration-300 ease-in-out" type="submit">Log In</button>
+                    </form>
+                    <div className="mt-6 text-center text-gray-800">
+                    </div>
                 </div>
-                <div>
-                    <input type="password" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                </div>
-                <button type="submit">Log In</button>
-            </form>
+            </div>
         </div>
     );
 };
