8dc966e3e5c82626e947ab0a9a7017a009f9cf0c
hana <rogovahana@gmail.com>
2024-04-25 23:54:18 +0200

    Create 'Single Line Page' Functionality

--- client/src/components/Line.jsx
+++ client/src/components/Line.jsx
@@ -1,7 +1,93 @@
-const Line = () => {
-    return(
-        <h1>Hello Line</h1>
-    );
+import React from 'react';
+import axios from 'axios';
+
+const Line = ({ onClose, schedule, departureDate, departureTime, arrivalDate, arrivalTime, totalPrice }) => {
+    // Function to handle reservation
+    const handleReservation = async () => {
+        const token = localStorage.getItem('token'); // Retrieve the token from local storage
+        const userId = localStorage.getItem('userId'); // Retrieve the token from local storage
+
+        if (!token) {
+
+            // If token is not found, handle unauthorized access (e.g., redirect to login page)
+            console.error('User is not authenticated.');
+            // Handle unauthorized access here, such as showing a message to the user or redirecting to the login page
+            return;
+        }
+
+        try {
+            // Make a POST request to create a ticket, passing the token as a bearer token in the Authorization header
+            const response = await axios.post(
+                'https://localhost:7264/Ticket',
+                {
+                    busScheduleId: schedule.id,
+                    userId: userId,
+                    dateOfBooking: new Date()
+                },
+                {
+                    headers: {
+                        Authorization: Bearer ${ token }
+                    }
+                }
+            );
+console.log('Ticket created:', response.data);
+onClose(); // Close the modal after successful reservation
+        } catch (error) {
+    console.error('Error creating ticket:', error.response.data);
 }
+    };
+
+return (
+    <div>
+        <div>
+            <div className="modal-content">
+                <h1>Detajet e linjes</h1>
+
+                <div>
+                    <p>Nisja</p>
+                    <p>{schedule.startCityName}</p>
+                </div>
+                <div>
+                    <p>Destinacioni</p>
+                    <p>{schedule.destinationCityName}</p>
+                </div>
+                <div>
+                    <p>Operatori</p>
+                    <p>{schedule.operatorName}</p>
+                    <br />
+                    <p>Data</p>
+                    <p>{departureDate}</p>
+                </div>
+                <div>
+                    <p>Ndalesat:</p>
+                    <p>
+                        <span>{departureTime}</span> -
+                        <span> {schedule.startCityName}</span>
+                    </p>
+                    {schedule.stationNames.map((station, idx) => (
+                        <p key={idx}>&#x25C8; {station} <br></br><span>&#x250B;</span></p>
+                    ))}
+                    <p>
+                        <span>{departureTime}</span> -
+                        <span> {schedule.destinationCityName}</span>
+                    </p>
+                </div>
+
+                <div>
+                    <p>
+                        <span>Cmimi:</span> -
+                        <span> {totalPrice} &#8364;</span>
+                    </p>
+                </div>
+
+                {/* Reserve and Close buttons */}
+                <button onClick={handleReservation}>Rezervo</button>
+                <button onClick={onClose}>Close</button>
+            </div>
+        </div>
+    </div>
+);
+
+};
 
 export default Line;
--- client/src/pages/Lines.jsx
+++ client/src/pages/Lines.jsx
@@ -1,8 +1,8 @@
-// Lines.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import NavBar from "../components/NavBar"
+import Line from '../components/Line';
 
 const Lines = () => {
     const location = useLocation();
@@ -13,6 +13,8 @@ const Lines = () => {
     const [departureTime, setDepartureTime] = useState(initialDepartureTime);
     const [cities, setCities] = useState([]);
     const [busSchedules, setBusSchedules] = useState([]);
+    const [selectedSchedule, setSelectedSchedule] = useState(null);
+    const [isLineOpen, setIsLineOpen] = useState(false);
 
     useEffect(() => {
         // Fetch cities API
@@ -45,61 +47,108 @@ const Lines = () => {
             });
     }, [startCity, destinationCity, departureTime]);
 
+    const handleDetailsClick = (schedule) => {
+        const depDate = new Date(schedule.departure).toLocaleDateString();
+        const depTime = new Date(schedule.departure).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        const arrDate = new Date(schedule.arrival).toLocaleDateString();
+        const arrTime = new Date(schedule.arrival).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+        // Calculate and format total price
+        const totalPrice = schedule.price * passengerCount;
+        const formattedTotalPrice = totalPrice;
+
+        console.log("departureDate:", depDate);
+        console.log("departureTime:", depTime);
+        console.log("arrivalDate:", arrDate);
+        console.log("arrivalTime:", arrTime);
+
+        if (selectedSchedule && selectedSchedule.id === schedule.id) {
+            // If the clicked schedule is already selected, close the line
+            setIsLineOpen(false);
+            setSelectedSchedule(null);
+        } else {
+            // If a different schedule is clicked, open the line with the new schedule
+            setSelectedSchedule({
+                ...schedule,
+                departureDate: depDate,
+                departureTime: depTime,
+                arrivalDate: arrDate,
+                arrivalTime: arrTime,
+                totalPrice: formattedTotalPrice,
+            });
+            setIsLineOpen(true);
+        }
+    };
+
+    const closeLine = () => {
+        setIsLineOpen(false);
+    };
+
     return (
         <>
             <div>
+                <div className='absolute right-0'>
+                    {selectedSchedule && isLineOpen && (
+                        <Line onClose={closeLine} schedule={selectedSchedule} departureDate={selectedSchedule.departureDate} departureTime={selectedSchedule.departureTime} arrivalDate={selectedSchedule.arrivalDate} arrivalTime={selectedSchedule.arrivalTime} totalPrice={selectedSchedule.totalPrice}>
+                        </Line>
+                    )}
+                </div>
                 <NavBar />
-                <div className="flex justify-center items-center mt-10">
-                    <div>
-                        <h3 className="text-2xl font-medium mb-10">Oraret e Autobusit</h3>
-                        <ul>
-                            {busSchedules.filter((schedule) => {
-                                const departureDate = new Date(schedule.departure);
-                                const currentDate = new Date();
-                                return departureDate.getTime() > currentDate.getTime();
-                            }).map((schedule) => {
-                                const dateTimeString = schedule.departure;
-                                const arrivalDateTimeString = schedule.arrival;
-                                const dateTime = new Date(dateTimeString);
-                                const arrivalDateTime = new Date(arrivalDateTimeString);
-                                const departureDate = dateTime.toLocaleDateString();
-                                const departureTime = dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-                                const arrivalDate = arrivalDateTime.toLocaleDateString();
-                                const arrivalTime = arrivalDateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+                <div className='flex flex-row-reverse justify-center'>
+                    <div className="flex justify-center items-center mt-10">
+                        <div>
+                            <h3 className="text-2xl font-medium mb-10">Oraret e Autobusit</h3>
+                            <ul>
+                                {busSchedules.filter((schedule) => {
+                                    const departureDate = new Date(schedule.departure);
+                                    const currentDate = new Date();
+                                    return departureDate.getTime() > currentDate.getTime();
+                                }).map((schedule) => {
+                                    const dateTimeString = schedule.departure;
+                                    const arrivalDateTimeString = schedule.arrival;
+                                    const dateTime = new Date(dateTimeString);
+                                    const arrivalDateTime = new Date(arrivalDateTimeString);
+                                    const departureDate = dateTime.toLocaleDateString();
+                                    const departureTime = dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+                                    const arrivalDate = arrivalDateTime.toLocaleDateString();
+                                    const arrivalTime = arrivalDateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-                                const totalPrice = schedule.price * passengerCount;
+                                    const totalPrice = schedule.price * passengerCount;
 
-                                return (
-                                    <div className="bg-white rounded-lg shadow-xl overflow-hidden w-[750px] mb-10">
-                                        <div className="flex items-center px-4 py-2 border-b border-gray-200 justify-between">
-                                            <h3 className="text-lg font-normal text-gray-900 mr-2">
-                                                <span className="text-orange-400">&#x21B3;</span>{schedule.startCityName}
-                                            </h3>
-                                            <div className='flex'>
-                                                <h3>{departureDate} at  {departureTime}</h3>
+                                    return (
+                                        <>
+                                            <div className="bg-white rounded-lg shadow-xl overflow-hidden w-[750px] mb-10 hover:bg-orange-50">
+                                                <div className="flex items-center px-4 py-2 border-b border-gray-200 justify-between">
+                                                    <h3 className="text-lg font-normal text-gray-900 mr-2">
+                                                        <span className="text-orange-400">&#x21B3;</span>{schedule.startCityName}
+                                                    </h3>
+                                                    <div className='flex'>
+                                                        <h3>{departureDate} at  {departureTime}</h3>
+                                                    </div>
+                                                </div>
+                                                <div className="flex items-center px-4 py-2 border-b border-gray-200 justify-between">
+                                                    <h3 className="text-lg font-normal text-gray-900 mr-2">
+                                                        <span className="text-orange-400">&#x21B3;</span>{schedule.destinationCityName}
+                                                    </h3>
+                                                    <div className='flex'>
+                                                        <h3>{arrivalDate} at  {arrivalTime}</h3>
+                                                    </div>
+                                                </div>
+                                                <div className="flex items-center px-4 py-2">
+                                                    <div>
+                                                        <p className="ml-auto text-sm text-orange-400">{totalPrice}&#8364;</p>
+                                                        <p className="text-gray-500 text-sm">{schedule.operatorName}</p>
+                                                    </div>
+                                                    <button className="text-sm focus:outline-none ml-auto text-orange-400 hover:text-orange-700" onClick={() => handleDetailsClick(schedule)}>
+                                                        Shiko detajet
+                                                    </button>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="flex items-center px-4 py-2 border-b border-gray-200 justify-between">
-                                            <h3 className="text-lg font-normal text-gray-900 mr-2">
-                                                <span className="text-orange-400">&#x21B3;</span>{schedule.destinationCityName}
-                                            </h3>
-                                            <div className='flex'>
-                                                <h3>{arrivalDate} at  {arrivalTime}</h3>
-                                            </div>
-                                        </div>
-                                        <div className="flex items-center px-4 py-2">
-                                            <div>
-                                                <p className="ml-auto text-sm text-orange-400">{totalPrice}&#8364;</p>
-                                                <p className="text-gray-500 text-sm">{schedule.operatorName}</p>
-                                            </div>
-                                            <button className="text-sm focus:outline-none ml-auto text-orange-400">
-                                                Shiko detajet
-                                            </button>
-                                        </div>
-                                    </div>
-                                );
-                            })}
-                        </ul>
+                                        </>
+                                    );
+                                })}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
